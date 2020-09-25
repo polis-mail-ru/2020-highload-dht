a492e3aa495d25ed56188d57ccdc5a3849e4024e
art241111 <art241111@mail.ru>
2020-09-25 10:11:12 +0300

    Add MyService.java and implements method at ServiceFactory

---
+++ src/main/java/ru/mail/polis/service/art241111/MyService.java
@@ -0,0 +1,15 @@
+package ru.mail.polis.service.art241111;
+
+import ru.mail.polis.service.Service;
+
+public class MyService implements Service {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
