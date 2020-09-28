5f516939f554be44bc262c761788541b05be3489
mission code now <missioncodenow@fakemail.com>
2020-09-28 12:36:27 +0300

    raw stuff - dependency error managed

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,9 @@ dependencies {
     // Annotations for better code documentation
     compile("com.intellij:annotations:12.0")
 
+    // third party solution to implement DAO
+    compile("org.rocksdb:rocksdbjni:5.8.0")
+
     // Guava primitives
     compile("com.google.guava:guava:27.0.1-jre")
 
