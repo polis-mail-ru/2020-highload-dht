84c7638f650526c05e84c717a3acbdbdb9297a77
Andrey Vladimirovich <45318753+JavaUnchained@users.noreply.github.com>
2020-09-29 15:33:19 +0300

    add description

--- src/main/java/ru/mail/polis/dao/kovalkov/Utils/BufferConverter.java
+++ src/main/java/ru/mail/polis/dao/kovalkov/Utils/BufferConverter.java
@@ -5,23 +5,24 @@ import org.jetbrains.annotations.NotNull;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
+/** Dont touch methods of these class
+ *  It all tests doesn't work for anyone without them.
+ *
+ * Vadim Tsesko knows about this problem
+ *
+ * Environment:
+ * RE version: OpenJDK Runtime Environment AdoptOpenJDK (11.0.8+10) (build 11.0.8+10)
+ * Java VM: OpenJDK 64-Bit Server VM AdoptOpenJDK (11.0.8+10, mixed mode, tiered,
+ * compressed oops, g1 gc, windows-amd64)
+ * Host: AMD Ryzen 7 4800HS with Radeon Graphics
+ * 16 cores, 15G,  Windows 10 , 64 bit Build 19041 (10.0.19041.292)
+ * Firewall: turned-off
+ *
+ * Use: ByteBuffer.wrap(copy); instead {@link #foldToBuffer(byte[])} {@link #convertBuffer(ByteBuffer)}
+ * Get: Unexpected exception thrown.
+ * org.gradle.internal.remote.internal.MessageIOException: Could not read message from '/127.0.0.1:62529'.
+ */
 public class BufferConverter {
-
-    /** Dont touch these methods
-     *  It all tests doesn't work for anyone without them.
-     *
-     * Environment:
-     * RE version: OpenJDK Runtime Environment AdoptOpenJDK (11.0.8+10) (build 11.0.8+10)
-     * Java VM: OpenJDK 64-Bit Server VM AdoptOpenJDK (11.0.8+10, mixed mode, tiered,
-     * compressed oops, g1 gc, windows-amd64)
-     * Host: AMD Ryzen 7 4800HS with Radeon Graphics
-     * 16 cores, 15G,  Windows 10 , 64 bit Build 19041 (10.0.19041.292)
-     * Firewall: turned-off
-     *
-     * Use: ByteBuffer.wrap(copy); instead {@link #foldToBuffer(byte[])} {@link #convertBuffer(ByteBuffer)}
-     * Get: Unexpected exception thrown.
-     * org.gradle.internal.remote.internal.MessageIOException: Could not read message from '/127.0.0.1:62529'.
-     */
 
     public static byte[] convertBuffer(@NotNull final ByteBuffer key) {
         final byte[] bytes = unfoldToBytes(key);
