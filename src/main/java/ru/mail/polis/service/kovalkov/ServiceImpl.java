e89a42cc924e5d6aa3340a69d2652397aebc3e48
JavaUnchained <AndreyVladimirov1ch@yandex.ru>
2020-10-02 23:43:35 +0300

    fix

--- src/main/java/ru/mail/polis/service/kovalkov/ServiceImpl.java
+++ src/main/java/ru/mail/polis/service/kovalkov/ServiceImpl.java
@@ -66,8 +66,7 @@ public class ServiceImpl extends HttpServer implements Service {
     @Path("/v0/entity")
     @RequestMethod(METHOD_GET)
     public Response get(@Param("id") final String id) {
-        if (id == null ||
-                id.isEmpty()) {
+        if (id == null || id.isEmpty()) {
             return new Response(Response.BAD_REQUEST, Response.EMPTY);
         }
         final ByteBuffer key = ByteBuffer.wrap(id.getBytes(UTF_8));
