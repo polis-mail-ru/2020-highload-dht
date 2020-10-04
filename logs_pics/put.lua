e5f2f3b148f823c16430ed0634992f8f0bfa73e3
Stas Tarasov <iluvstrwrs@mail.ru>
2020-10-04 16:32:51 +0300

    added new put lua

--- logs_pics/put.lua
+++ logs_pics/put.lua
@@ -1,6 +1,14 @@
 -- example HTTP POST script which demonstrates setting the
 -- HTTP method, body, and adding a header
 
-wrk.method = "PUT"
-wrk.body = "123"
-wrk.headers["Content-Type"] = "text/plain"
+
+key = 0;
+
+request = function()
+    path = "/v0/entity?id=" .. key
+    wrk.method = "PUT"
+    wrk.body = "123"
+    wrk.headers["Content-Type"] = "text/plain"
+    key = key + 1
+    return wrk.format(nil, path)
+end
