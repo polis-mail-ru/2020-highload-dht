3ce61c75b429918bab9f71bfe522bbd2c462f96d
stakenschneider <volkova.maria.d@gmail.com>
2020-10-05 04:36:44 +0300

    change lua scripts

--- wrk/delete.lua
+++ wrk/delete.lua
@@ -1,7 +1,7 @@
 counter = 0
 
 request = function()
-    path="/v0/entity?id=key" .. counter .. "&replicas=3/3"
+    path="/v0/entity?id=" .. counter .. "&replicas=3/3"
     wrk.method = "DELETE"
     counter = counter + 1
     return wrk.format(nil, path)
--- wrk/get.lua
+++ wrk/get.lua
@@ -1,7 +1,7 @@
 counter = 0
 
 request = function()
-    path="/v0/entity?id=key" .. counter .. "&replicas=3/3"
+    path="/v0/entity?id=" .. counter
     wrk.method = "GET"
     counter = counter + 1
     return wrk.format(nil, path)
