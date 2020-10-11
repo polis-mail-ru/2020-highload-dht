7be4cb7a9536b65890d6de2f4d0130b26447deee
JavaUnchained <AndreyVladimirov1ch@yandex.ru>
2020-10-11 18:19:03 +0300

    complete report

--- src/main/java/ru/mail/polis/service/ServiceFactory.java
+++ src/main/java/ru/mail/polis/service/ServiceFactory.java
@@ -19,6 +19,7 @@ package ru.mail.polis.service;
 import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.dao.DAO;
 import ru.mail.polis.service.kovalkov.AsyncServiceImpl;
+import ru.mail.polis.service.kovalkov.ServiceImpl;
 
 import java.io.IOException;
 
@@ -53,6 +54,7 @@ public final class ServiceFactory {
             throw new IllegalArgumentException("Port out of range");
         }
 
+//        return new ServiceImpl(ServiceImpl.getConfig(port), dao);
         return new AsyncServiceImpl(AsyncServiceImpl.getConfig(port), dao);
     }
 }
