e5b179aac4587dd63d2bdc24f128456fc3c8f37d
Shlyaga Vyacheslav <slava.shlyaga@mail.ru>
2020-10-14 02:21:53 +0300

    implement async service

--- src/main/java/ru/mail/polis/service/ServiceFactory.java
+++ src/main/java/ru/mail/polis/service/ServiceFactory.java
@@ -53,6 +53,6 @@ public final class ServiceFactory {
             throw new IllegalArgumentException("Port out of range");
         }
 
-        return BasicService.of(port, dao, Runtime.getRuntime().availableProcessors());
+        return BasicService.of(port, dao, Runtime.getRuntime().availableProcessors(), 5000);
     }
 }
--- src/main/java/ru/mail/polis/service/s3ponia/BasicService.java
+++ src/main/java/ru/mail/polis/service/s3ponia/BasicService.java
@@ -20,9 +20,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.NoSuchElementException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public final class BasicService extends HttpServer implements Service {
     private static final Logger logger = LoggerFactory.getLogger(BasicService.class);
@@ -30,15 +28,21 @@ public final class BasicService extends HttpServer implements Service {
     private final DAO dao;
     private final ExecutorService es;
 
-    private BasicService(final int port, @NotNull final DAO dao, final int workers) throws IOException {
+    private BasicService(final int port, @NotNull final DAO dao,
+                         final int workers, final int queueSize) throws IOException {
         super(configFrom(port));
+        assert 0 < workers;
+        assert 0 < queueSize;
         this.dao = dao;
-        this.es = Executors.newFixedThreadPool(workers,
+        this.es = new ThreadPoolExecutor(workers, workers,
+                0L, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(queueSize),
                 new ThreadFactoryBuilder()
                         .setNameFormat("worker-%d")
                         .setUncaughtExceptionHandler((t, e) ->
                                 logger.error("Error in {} when processing request", t, e))
-                        .build());
+                        .build(),
+                new ThreadPoolExecutor.AbortPolicy());
     }
 
     @NotNull
@@ -60,19 +64,29 @@ public final class BasicService extends HttpServer implements Service {
     }
 
     @NotNull
-    public static BasicService of(final int port, @NotNull final DAO dao, final int workers) throws IOException {
-        return new BasicService(port, dao, workers);
+    public static BasicService of(final int port, @NotNull final DAO dao, final int workers, final int queueSize) throws IOException {
+        return new BasicService(port, dao, workers, queueSize);
     }
 
     @Path("/v0/status")
     public void handleStatus(final HttpSession session) throws IOException {
-        this.es.execute(() -> {
+        try {
+            this.es.execute(() -> {
+                try {
+                    session.sendResponse(Response.ok("OK"));
+                    Thread.sleep(10);
+                } catch (Exception e) {
+                    logger.error("Error in sending status", e);
+                }
+            });
+        } catch (Exception e) {
+            logger.error("Can't schedule request for execution", e);
             try {
-                session.sendResponse(Response.ok("OK"));
-            } catch (IOException e) {
-                logger.error("Error in sending status", e);
+                session.sendResponse(new Response(Response.SERVICE_UNAVAILABLE));
+            } catch (IOException ioException) {
+                logger.error("Can't send internal error", ioException);
             }
-        });
+        }
     }
 
     /**
@@ -83,22 +97,49 @@ public final class BasicService extends HttpServer implements Service {
      */
     @Path("/v0/entity")
     @RequestMethod(Request.METHOD_GET)
-    public Response handleGet(@Param(value = "id", required = true) final String id) {
-        if (id.isEmpty()) {
-            logger.error("Empty key in getting");
-            return new Response(Response.BAD_REQUEST, EMPTY);
-        }
-
-        final ByteBuffer buffer = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
+    public void handleGet(@Param(value = "id", required = true) final String id, final HttpSession session) {
         try {
-            final ByteBuffer value = dao.get(buffer);
-            return Response.ok(fromByteBuffer(value));
-        } catch (NoSuchElementException e) {
-            logger.error("No such element key(size: {}) in dao", id.length());
-            return new Response(Response.NOT_FOUND, EMPTY);
-        } catch (IOException e) {
-            logger.error("IOException in getting key(size: {}) from dao", id.length());
-            return new Response(Response.INTERNAL_ERROR, EMPTY);
+            this.es.execute(() -> {
+                if (id.isEmpty()) {
+                    logger.error("Empty key in getting");
+                    try {
+                        session.sendResponse(new Response(Response.BAD_REQUEST, EMPTY));
+                    } catch (IOException e) {
+                        logger.error("Error in sending error", e);
+                    }
+                }
+
+                final ByteBuffer buffer = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
+                try {
+                    final ByteBuffer value = dao.get(buffer);
+                    try {
+                        session.sendResponse(Response.ok(fromByteBuffer(value)));
+                    } catch (IOException ioException) {
+                        logger.error("Error in sending response", ioException);
+                    }
+                } catch (NoSuchElementException e) {
+                    logger.error("No such element key(size: {}) in dao", id.length());
+                    try {
+                        session.sendResponse(new Response(Response.NOT_FOUND, EMPTY));
+                    } catch (IOException ioException) {
+                        logger.error("Error in sending error", ioException);
+                    }
+                } catch (IOException e) {
+                    logger.error("IOException in getting key(size: {}) from dao", id.length());
+                    try {
+                        session.sendResponse(new Response(Response.INTERNAL_ERROR, EMPTY));
+                    } catch (IOException ioException) {
+                        logger.error("Error in sending error", ioException);
+                    }
+                }
+            });
+        } catch (Exception e) {
+            logger.error("Can't schedule request for execution", e);
+            try {
+                session.sendResponse(new Response(Response.SERVICE_UNAVAILABLE));
+            } catch (IOException ioException) {
+                logger.error("Can't send internal error", ioException);
+            }
         }
     }
 
@@ -111,19 +152,43 @@ public final class BasicService extends HttpServer implements Service {
      */
     @Path("/v0/entity")
     @RequestMethod(Request.METHOD_PUT)
-    public Response handlePut(@Param(value = "id", required = true) final String id, final Request request) {
-        if (id.isEmpty()) {
-            logger.error("Empty key in putting");
-            return new Response(Response.BAD_REQUEST, EMPTY);
-        }
-
-        final ByteBuffer buffer = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
+    public Response handlePut(@Param(value = "id", required = true) final String id, final Request request,
+                              final HttpSession session) {
         try {
-            dao.upsert(buffer, ByteBuffer.wrap(request.getBody()));
-            return new Response(Response.CREATED, EMPTY);
-        } catch (IOException e) {
-            logger.error("IOException in putting key(size: {}) from dao", id.length());
-            return new Response(Response.INTERNAL_ERROR, EMPTY);
+            this.es.execute(() -> {
+                if (id.isEmpty()) {
+                    logger.error("Empty key in putting");
+                    try {
+                        session.sendResponse(new Response(Response.BAD_REQUEST, EMPTY));
+                    } catch (IOException e) {
+                        logger.error("Error in sending error", e);
+                    }
+                }
+
+                final ByteBuffer buffer = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
+                try {
+                    dao.upsert(buffer, ByteBuffer.wrap(request.getBody()));
+                    try {
+                        session.sendResponse(new Response(Response.CREATED, EMPTY));
+                    } catch (IOException ioException) {
+                        logger.error("Error in sending response", ioException);
+                    }
+                } catch (IOException e) {
+                    logger.error("IOException in putting key(size: {}) from dao", id.length());
+                    try {
+                        session.sendResponse(new Response(Response.INTERNAL_ERROR, EMPTY));
+                    } catch (IOException ioException) {
+                        logger.error("Error in sending error", ioException);
+                    }
+                }
+            });
+        } catch (Exception e) {
+            logger.error("Can't schedule request for execution", e);
+            try {
+                session.sendResponse(new Response(Response.SERVICE_UNAVAILABLE));
+            } catch (IOException ioException) {
+                logger.error("Can't send internal error", ioException);
+            }
         }
     }
 
@@ -135,19 +200,43 @@ public final class BasicService extends HttpServer implements Service {
      */
     @Path("/v0/entity")
     @RequestMethod(Request.METHOD_DELETE)
-    public Response handleDelete(@Param(value = "id", required = true) final String id) {
-        if (id.isEmpty()) {
-            logger.error("Empty key in deleting");
-            return new Response(Response.BAD_REQUEST, EMPTY);
-        }
-
-        final ByteBuffer buffer = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
+    public Response handleDelete(@Param(value = "id", required = true) final String id, final HttpSession session) {
         try {
-            dao.remove(buffer);
-            return new Response(Response.ACCEPTED, EMPTY);
-        } catch (IOException e) {
-            logger.error("IOException in removing key(size: {}) from dao", id.length());
-            return new Response(Response.INTERNAL_ERROR, EMPTY);
+            this.es.execute(() -> {
+                if (id.isEmpty()) {
+                    logger.error("Empty key in deleting");
+                    try {
+                        session.sendResponse(new Response(Response.BAD_REQUEST, EMPTY));
+                    } catch (IOException e) {
+                        logger.error("Error in sending error", e);
+                    }
+                }
+
+                final ByteBuffer buffer = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
+                try {
+                    dao.remove(buffer);
+                    try {
+                        session.sendResponse(new Response(Response.ACCEPTED, EMPTY));
+                    } catch (IOException ioException) {
+                        logger.error("Error in sending response", ioException);
+                    }
+                } catch (IOException e) {
+                    logger.error("IOException in removing key(size: {}) from dao", id.length());
+                    try {
+                        session.sendResponse(new Response(Response.INTERNAL_ERROR, EMPTY));
+                    } catch (IOException ioException) {
+                        logger.error("Error in sending error", ioException);
+                    }
+                }
+
+            });
+        } catch (Exception e) {
+            logger.error("Can't schedule request for execution", e);
+            try {
+                session.sendResponse(new Response(Response.SERVICE_UNAVAILABLE));
+            } catch (IOException ioException) {
+                logger.error("Can't send internal error", ioException);
+            }
         }
     }
 
