0337ec3abfc745a4a1fcb613bbbf424a629f8464
Vlad Zybkin <zybkin11@gmail.com>
2020-10-16 12:21:57 +0300

    cache

--- src/main/java/ru/mail/polis/service/zvladn7/AsyncService.java
+++ src/main/java/ru/mail/polis/service/zvladn7/AsyncService.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.service.zvladn7;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
@@ -20,27 +23,30 @@ import ru.mail.polis.service.Service;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class AsyncService extends HttpServer implements Service {
 
     private static final String ERROR_SENDING_RESPONSE = "Error when sending response";
     private static final String ERROR_SERVICE_UNAVAILABLE = "Cannot send SERVICE_UNAVAILABLE response";
     private static final Logger log = LoggerFactory.getLogger(AsyncService.class);
-    private static final int CACHE_SIZE = 15;
 
     private final DAO dao;
-    private final Map<String, byte[]> cache = new LinkedHashMap<>();
-    private final ReentrantLock lock = new ReentrantLock();
+    private Cache<String, byte[]> cache = CacheBuilder.newBuilder()
+            .initialCapacity(1024)
+            .concurrencyLevel(8)
+            .removalListener((RemovalListener<String, byte[]>) notification -> {
+                log.debug("Remove from cache with key: " + notification.getKey());
+                log.debug("Cause: " + notification.getCause().name());
+            })
+            .maximumSize(1024)
+            .expireAfterAccess(1, TimeUnit.MINUTES)
+            .build();
     private final ExecutorService es;
 
     /**
@@ -68,6 +74,159 @@ public class AsyncService extends HttpServer implements Service {
         );
     }
 
+    @Override
+    public void handleDefault(final Request request, final HttpSession session) throws IOException {
+        log.error("Unsupported mapping request.\n Cannot understand it: {} {}",
+                request.getMethodName(), request.getPath());
+        session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
+    }
+
+    /**
+     * Return status of the server instance.
+     *
+     * @param session - server session
+     */
+    @Path("/v0/status")
+    public void status(final HttpSession session) {
+        processRequest(() -> session.sendResponse(Response.ok("Status: OK")), session);
+    }
+
+    /**
+     * This method get value with provided id.
+     * Async response can have different values which depend on the key or io errors.
+     * Values:
+     * 1. 200 OK. Also return body.
+     * 2. 400 if id is empty
+     * 3. 404 if value with id was not found
+     * 4. 500 if some io error was happened
+     *
+     * @param id - String
+     */
+    @Path("/v0/entity")
+    @RequestMethod(Request.METHOD_GET)
+    public void get(@Param(value = "id", required = true) final String id,
+                    final HttpSession session) {
+        processRequest(() -> handleGet(id, session), session);
+    }
+
+    /**
+     * This method delete value with provided id.
+     * Async response can have different values which depend on the key or io errors.
+     * Values:
+     * 1. 202 if value is successfully deleted
+     * 2. 400 if id is empty
+     * 3. 500 if some io error was happened
+     *
+     * @param id - String
+     */
+    @Path("/v0/entity")
+    @RequestMethod(Request.METHOD_DELETE)
+    public void remove(@Param(value = "id", required = true) final String id,
+                       final HttpSession session) {
+        processRequest(() -> handleDelete(id, session), session);
+    }
+
+    /**
+     * This method insert or update value with provided id.
+     * Async response can have different values which depend on the key or io errors.
+     * Values:
+     * 1. 201 if value is successfully inserted and created
+     * 2. 400 if id is empty
+     * 3. 500 if some io error was happened
+     *
+     * @param id - String
+     */
+    @Path("/v0/entity")
+    @RequestMethod(Request.METHOD_PUT)
+    public void upsert(
+            @Param(value = "id", required = true) final String id,
+            final Request request,
+            final HttpSession session) {
+        processRequest(() -> handleUpsert(id, request, session), session);
+    }
+
+    @Override
+    public synchronized void stop() {
+        super.stop();
+        es.shutdown();
+        try {
+            es.awaitTermination(30, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            log.error("Error when trying to stop executor service");
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void handleGet(
+            @NotNull final String id,
+            @NotNull final HttpSession session) throws IOException {
+        log.debug("GET request with mapping: /v0/entity and key={}", id);
+        if (id.isEmpty()) {
+            sendEmptyIdResponse(session);
+            return;
+        }
+        byte[] body = cache.getIfPresent(id);
+        if (body == null) {
+            final ByteBuffer key = wrapString(id);
+            final ByteBuffer value;
+            try {
+                value = dao.get(key);
+            } catch (NoSuchElementException e) {
+                log.info("Value with key: {} was not found", id, e);
+                session.sendResponse(new Response(Response.NOT_FOUND, Response.EMPTY));
+                return;
+            } catch (IOException e) {
+                sendInternalErrorResponse(session, id, e);
+                return;
+            }
+            body = toBytes(value);
+            cache.put(id, body);
+        }
+        session.sendResponse(Response.ok(body));
+    }
+
+    private void handleDelete(
+            @NotNull final String id,
+            @NotNull final HttpSession session) throws IOException {
+        log.debug("DELETE request with mapping: /v0/entity and key={}", id);
+        if (id.isEmpty()) {
+            sendEmptyIdResponse(session);
+            return;
+        }
+        final ByteBuffer key = wrapString(id);
+        try {
+            dao.remove(key);
+            cache.asMap().remove(id);
+        } catch (IOException e) {
+            sendInternalErrorResponse(session, id, e);
+            return;
+        }
+        session.sendResponse(new Response(Response.ACCEPTED, Response.EMPTY));
+    }
+
+    private void handleUpsert(
+            @NotNull final String id,
+            @NotNull final Request request,
+            @NotNull final HttpSession session) throws IOException {
+        log.debug("PUT request with mapping: /v0/entity with: key={}, value={}",
+                id, new String(request.getBody(), StandardCharsets.UTF_8));
+        if (id.isEmpty()) {
+            sendEmptyIdResponse(session);
+            return;
+        }
+
+        final ByteBuffer key = wrapString(id);
+        final ByteBuffer value = wrapArray(request.getBody());
+        try {
+            dao.upsert(key, value);
+            cache.asMap().computeIfPresent(id, (k, v) -> request.getBody());
+        } catch (IOException e) {
+            sendInternalErrorResponse(session, id, e);
+            return;
+        }
+        session.sendResponse(new Response(Response.CREATED, Response.EMPTY));
+    }
+
     private static HttpServerConfig provideConfig(final int port) {
         final AcceptorConfig acceptorConfig = new AcceptorConfig();
         acceptorConfig.port = port;
@@ -89,15 +248,6 @@ public class AsyncService extends HttpServer implements Service {
         }
 
         return Response.EMPTY;
-    }
-
-    private void updateCache(final String key, final byte[] value) {
-        if (cache.size() >= CACHE_SIZE) {
-            final Iterator<Map.Entry<String, byte[]>> iterator = cache.entrySet().iterator();
-            iterator.next();
-            iterator.remove();
-        }
-        cache.put(key, value);
     }
 
     private static ByteBuffer wrapString(final String str) {
@@ -142,162 +292,6 @@ public class AsyncService extends HttpServer implements Service {
             es.execute(() -> process(processor));
         } catch (RejectedExecutionException e) {
             sendServiceUnavailableResponse(session);
-        }
-    }
-
-    @Override
-    public void handleDefault(final Request request, final HttpSession session) throws IOException {
-        log.error("Unsupported mapping request.\n Cannot understand it: {} {}",
-                request.getMethodName(), request.getPath());
-        session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
-    }
-
-    /**
-     * Return status of the server instance.
-     *
-     * @param session - server session
-     */
-    @Path("/v0/status")
-    public void status(final HttpSession session) {
-        processRequest(() -> session.sendResponse(Response.ok("Status: OK")), session);
-    }
-
-    /**
-     * This method get value with provided id.
-     * Async response can have different values which depend on the key or io errors.
-     * Values:
-     * 1. 200 OK. Also return body.
-     * 2. 400 if id is empty
-     * 3. 404 if value with id was not found
-     * 4. 500 if some io error was happened
-     *
-     * @param id - String
-     */
-    @Path("/v0/entity")
-    @RequestMethod(Request.METHOD_GET)
-    public void get(@Param(value = "id", required = true) final String id,
-                    final HttpSession session) {
-        processRequest(() -> {
-            log.debug("GET request with mapping: /v0/entity and key={}", id);
-            if (id.isEmpty()) {
-                sendEmptyIdResponse(session);
-                return;
-            }
-            byte[] body;
-            lock.lock();
-            try {
-                body = cache.get(id);
-            } finally {
-                lock.unlock();
-            }
-            if (body == null) {
-                final ByteBuffer key = wrapString(id);
-                final ByteBuffer value;
-                try {
-                    value = dao.get(key);
-                } catch (NoSuchElementException e) {
-                    log.info("Value with key: {} was not found", id, e);
-                    session.sendResponse(new Response(Response.NOT_FOUND, Response.EMPTY));
-                    return;
-                } catch (IOException e) {
-                    sendInternalErrorResponse(session, id, e);
-                    return;
-                }
-                body = toBytes(value);
-                final byte[] finalBody = body;
-                lockAction(() -> updateCache(id, finalBody));
-            }
-            session.sendResponse(Response.ok(body));
-        }, session);
-    }
-
-    /**
-     * This method delete value with provided id.
-     * Async response can have different values which depend on the key or io errors.
-     * Values:
-     * 1. 202 if value is successfully deleted
-     * 2. 400 if id is empty
-     * 3. 500 if some io error was happened
-     *
-     * @param id - String
-     */
-    @Path("/v0/entity")
-    @RequestMethod(Request.METHOD_DELETE)
-    public void remove(@Param(value = "id", required = true) final String id,
-                       final HttpSession session) {
-        processRequest(() -> {
-            log.debug("DELETE request with mapping: /v0/entity and key={}", id);
-            if (id.isEmpty()) {
-                sendEmptyIdResponse(session);
-                return;
-            }
-            final ByteBuffer key = wrapString(id);
-            try {
-                dao.remove(key);
-                lockAction(() -> cache.remove(id));
-            } catch (IOException e) {
-                sendInternalErrorResponse(session, id, e);
-                return;
-            }
-            session.sendResponse(new Response(Response.ACCEPTED, Response.EMPTY));
-        }, session);
-    }
-
-    /**
-     * This method insert or update value with provided id.
-     * Async response can have different values which depend on the key or io errors.
-     * Values:
-     * 1. 201 if value is successfully inserted and created
-     * 2. 400 if id is empty
-     * 3. 500 if some io error was happened
-     *
-     * @param id - String
-     */
-    @Path("/v0/entity")
-    @RequestMethod(Request.METHOD_PUT)
-    public void upsert(
-            @Param(value = "id", required = true) final String id,
-            final Request request,
-            final HttpSession session) {
-        processRequest(() -> {
-            log.debug("PUT request with mapping: /v0/entity with: key={}, value={}",
-                    id, new String(request.getBody(), StandardCharsets.UTF_8));
-            if (id.isEmpty()) {
-                sendEmptyIdResponse(session);
-                return;
-            }
-
-            final ByteBuffer key = wrapString(id);
-            final ByteBuffer value = wrapArray(request.getBody());
-            try {
-                dao.upsert(key, value);
-                lockAction(() -> cache.computeIfPresent(id, (k, v) -> request.getBody()));
-            } catch (IOException e) {
-                sendInternalErrorResponse(session, id, e);
-                return;
-            }
-            session.sendResponse(new Response(Response.CREATED, Response.EMPTY));
-        }, session);
-    }
-
-    @Override
-    public synchronized void stop() {
-        super.stop();
-        es.shutdown();
-        try {
-            es.awaitTermination(30, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            log.error("Error when trying to stop executor service");
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    private void lockAction(final Runnable action) {
-        lock.lock();
-        try {
-            action.run();
-        } finally {
-            lock.unlock();
         }
     }
 }
