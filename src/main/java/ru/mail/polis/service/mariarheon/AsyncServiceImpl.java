5398ea56fb71369e0ed71c6f235638f7e85a11eb
Maria Rheon <mikhspbstu@gmail.com>
2020-10-17 10:47:04 +0300

    codeclimate fixes p.13

--- src/main/java/ru/mail/polis/service/mariarheon/AsyncServiceImpl.java
+++ src/main/java/ru/mail/polis/service/mariarheon/AsyncServiceImpl.java
@@ -1,11 +1,6 @@
 package ru.mail.polis.service.mariarheon;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;
@@ -25,6 +20,11 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.NoSuchElementException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import static one.nio.http.Request.METHOD_DELETE;
 import static one.nio.http.Request.METHOD_GET;
