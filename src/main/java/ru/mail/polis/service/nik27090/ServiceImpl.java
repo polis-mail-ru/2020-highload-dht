649387d3b8d8f487103607e5adaa00c9e4534f51
n.khomenko <n.khomenko@just-ai.com>
2020-10-18 19:39:20 +0300

    codeclimate, srsly?

--- src/main/java/ru/mail/polis/service/nik27090/ServiceImpl.java
+++ src/main/java/ru/mail/polis/service/nik27090/ServiceImpl.java
@@ -215,9 +215,9 @@ public class ServiceImpl extends HttpServer implements Service {
     @Path("/v0/entity")
     @RequestMethod(Request.METHOD_DELETE)
     public void deleteEntity(
+            final Request request,
             @Param(value = "id", required = true) final String id,
-            final HttpSession session,
-            final Request request) {
+            final HttpSession session) {
         log.debug("DELETE request: id = {}", id);
 
         try {
