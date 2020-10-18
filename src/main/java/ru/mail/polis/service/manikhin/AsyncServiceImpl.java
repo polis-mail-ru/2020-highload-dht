722501bfcb677ad24f07b0ad0d587dd8da68ad17
Igor-Manikhin <imanihin822@gmail.com>
2020-10-18 18:26:05 +0300

    Fixed codeclimate

--- src/main/java/ru/mail/polis/service/manikhin/AsyncServiceImpl.java
+++ src/main/java/ru/mail/polis/service/manikhin/AsyncServiceImpl.java
@@ -22,10 +22,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.NoSuchElementException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.Future;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class AsyncServiceImpl extends HttpServer implements Service {
 
