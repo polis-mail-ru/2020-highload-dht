304236a04da9ec27f463f5a2387f6e9434bccb64
IvanovAndrey <olifem2012@gmail.com>
2020-10-19 19:47:13 +0300

    Codeclimate fix 3

--- src/main/java/ru/mail/polis/service/ivanovandrey/AsyncServiceImpl.java
+++ src/main/java/ru/mail/polis/service/ivanovandrey/AsyncServiceImpl.java
@@ -149,7 +149,7 @@ public class AsyncServiceImpl extends HttpServer implements Service {
                     default:
                         session.sendError(Response.METHOD_NOT_ALLOWED, "Wrong method");
                         break;
-                };
+                }
         } catch (IOException ex) {
             log.error("Error in ServiceImpl.get() method; internal error: ", ex);
             session.sendResponse(new Response(Response.INTERNAL_ERROR, Response.EMPTY));
