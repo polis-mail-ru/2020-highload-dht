58934b49314aa5bd6b962ef2acc4d969aaad4d8c
Maria Rheon <mikhspbstu@gmail.com>
2020-10-19 09:17:54 +0300

    codeclimate fixes p.18

--- src/main/java/ru/mail/polis/service/mariarheon/RendezvousSharding.java
+++ src/main/java/ru/mail/polis/service/mariarheon/RendezvousSharding.java
@@ -10,7 +10,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
