50cb2f13f47efb5d397a7962fd91ab8ff48a1b55
nkhomenko <nik27090@mail.ru>
2020-10-19 10:50:08 +0300

    Revert "codeclimate, srsly?"
    This reverts commit 649387d3

--- src/main/java/ru/mail/polis/service/nik27090/ServiceImpl.java
+++ src/main/java/ru/mail/polis/service/nik27090/ServiceImpl.java
@@ -215,9 +215,9 @@ public class ServiceImpl extends HttpServer implements Service {
     @Path("/v0/entity")
     @RequestMethod(Request.METHOD_DELETE)
     public void deleteEntity(
-            final Request request,
             @Param(value = "id", required = true) final String id,
-            final HttpSession session) {
+            final HttpSession session,
+            final Request request) {
         log.debug("DELETE request: id = {}", id);
 
         try {
