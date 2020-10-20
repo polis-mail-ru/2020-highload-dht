0a27ad299423ce5152edd6983d46d74b79a6da9c
JavaUnchained <AndreyVladimirov1ch@yandex.ru>
2020-10-20 10:45:44 +0300

    fix

--- src/main/java/ru/mail/polis/service/kovalkov/SharedAsyncServiceImpl.java
+++ src/main/java/ru/mail/polis/service/kovalkov/SharedAsyncServiceImpl.java
@@ -84,6 +84,7 @@ public class SharedAsyncServiceImpl extends HttpServer implements Service {
         acceptorConfig.reusePort = true;
         final HttpServerConfig httpServerConfig = new HttpServerConfig();
         httpServerConfig.maxWorkers = Runtime.getRuntime().availableProcessors();
+        httpServerConfig.queueTime = 10;
         httpServerConfig.acceptors = new AcceptorConfig[]{acceptorConfig};
         return httpServerConfig;
     }
