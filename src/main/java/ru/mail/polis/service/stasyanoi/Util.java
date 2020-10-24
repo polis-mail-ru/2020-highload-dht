1ceb7ab8a718b591a9b06ea48d3be4bf7df23cae
Stas Tarasov <iluvstrwrs@mail.ru>
2020-10-24 12:20:26 +0300

    fixes

--- src/main/java/ru/mail/polis/service/stasyanoi/Util.java
+++ src/main/java/ru/mail/polis/service/stasyanoi/Util.java
@@ -17,6 +17,12 @@ public class Util {
 
     private static final Logger logger = LoggerFactory.getLogger(Util.class);
 
+    /**
+     * Returns an empty bodied response
+     *
+     * @param requestType - type of response to return
+     * @return the response
+     */
     @NotNull
     public static Response getResponseWithNoBody(final String requestType) {
         final Response responseHttp = new Response(requestType);
@@ -24,6 +30,12 @@ public class Util {
         return responseHttp;
     }
 
+    /**
+     * Sends 500 error.
+     *
+     * @param session - to which to send the error .
+     * @param e - the exception that caused the error.
+     */
     public static void sendErrorInternal(final HttpSession session,
                                    final IOException e) {
         try {
