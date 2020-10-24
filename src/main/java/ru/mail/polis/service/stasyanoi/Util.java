7d07314fd190cc0f4ebfb48298c981bbf076f243
Stas Tarasov <iluvstrwrs@mail.ru>
2020-10-24 21:34:49 +0300

    checkstyle

--- src/main/java/ru/mail/polis/service/stasyanoi/Util.java
+++ src/main/java/ru/mail/polis/service/stasyanoi/Util.java
@@ -17,14 +17,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public final class Util {
 
