825e7f14185c64ba92286792a74aaf82a45aec6d
Stas Tarasov <iluvstrwrs@mail.ru>
2020-11-01 00:16:14 +0300

    new logic

--- src/main/java/ru/mail/polis/service/stasyanoi/CustomServer.java
+++ src/main/java/ru/mail/polis/service/stasyanoi/CustomServer.java
@@ -43,7 +43,9 @@ public class CustomServer extends FrameServer {
      */
     @Path("/v0/entity")
     @RequestMethod(Request.METHOD_GET)
-    public void get(final @Param("id") String idParam, final HttpSession session, final Request request) {
+    public void get(final @Param("id") String idParam,
+                    final HttpSession session,
+                    final Request request) {
         executorService.execute(() -> {
             try {
                 getInternal(idParam, session, request);
@@ -63,7 +65,8 @@ public class CustomServer extends FrameServer {
      */
     @Path("/v0/entity/rep")
     @RequestMethod(Request.METHOD_GET)
-    public void getRep(final @Param("id") String idParam, final HttpSession session) {
+    public void getRep(final @Param("id") String idParam,
+                       final HttpSession session) {
         executorService.execute(() -> {
             try {
                 getRepInternal(idParam, session);
@@ -75,7 +78,8 @@ public class CustomServer extends FrameServer {
         });
     }
 
-    private void getRepInternal(final String idParam, final HttpSession session) throws IOException {
+    private void getRepInternal(final String idParam,
+                                final HttpSession session) throws IOException {
         final Response responseHttp;
         if (idParam == null || idParam.isEmpty()) {
             responseHttp = Util.getResponseWithNoBody(Response.BAD_REQUEST);
@@ -86,7 +90,8 @@ public class CustomServer extends FrameServer {
         session.sendResponse(responseHttp);
     }
 
-    private void getInternal(final String idParam, final HttpSession session,
+    private void getInternal(final String idParam,
+                             final HttpSession session,
                              final Request request) throws IOException {
         final Response responseHttp;
         final Map<Integer, String> tempNodeMapping = new TreeMap<>(nodeMapping);
