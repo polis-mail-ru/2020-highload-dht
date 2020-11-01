dd360e92f15f073996e2f7a1824aaeb290a64c93
Stas Tarasov <iluvstrwrs@mail.ru>
2020-11-01 20:42:49 +0300

    refactor

--- src/main/java/ru/mail/polis/service/stasyanoi/server/internal/ConstantsServer.java
+++ src/main/java/ru/mail/polis/service/stasyanoi/server/internal/ConstantsServer.java
@@ -9,9 +9,10 @@ import java.util.List;
 
 public class ConstantsServer extends HttpServer {
 
-    protected static final String TRUE_VAL = "true";
-    protected static final String REPS = "reps";
-    protected static final List<String> replicationDefaults = Arrays.asList("1/1", "2/2", "2/3", "3/4", "3/5");
+    protected final String TRUE_VAL = "true";
+    protected final String REPS = "reps";
+    protected final List<String> replicationDefaults = Arrays.asList("1/1", "2/2", "2/3", "3/4", "3/5");
+
 
     public ConstantsServer(final HttpServerConfig config) throws IOException {
         super(config);
