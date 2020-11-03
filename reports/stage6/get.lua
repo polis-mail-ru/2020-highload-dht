65e3b975a9defc193445ea37cf15e98dbcf29ce9
Stas Tarasov <iluvstrwrs@mail.ru>
2020-11-04 01:34:04 +0300

    summary

---
+++ reports/stage6/get.lua
@@ -0,0 +1,12 @@
+-- example HTTP POST script which demonstrates setting the
+-- HTTP method, body, and adding a header
+
+
+key = 0;
+
+request = function()
+    path = "/v0/entity?id=" .. key
+    wrk.method = "GET"
+    key = key + 1
+    return wrk.format(nil, path)
+end
