135e54f310ed0a1a174372d1472a258494e54d2a
s3ponia <slava.shlyaga@mail.ru>
2020-11-08 08:47:53 +0300

    add lock get

--- test-results/task5/wrk2-scripts/get-request.lua
+++ test-results/task5/wrk2-scripts/get-request.lua
@@ -1,7 +1,6 @@
-count = math.random(3000)
+count = 0
 
 request = function() 
-  math.randomseed(os.time())
   path = "/v0/entity?id=key" .. count .. "&replicas=2/3"
   wrk.method = "GET"
   return wrk.format(nil, path)
