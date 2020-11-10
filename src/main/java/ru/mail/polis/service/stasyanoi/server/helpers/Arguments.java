3f0b38e428455b516f3faa1bbf4a7bbf27e13a1c
Stas Tarasov <iluvstrwrs@mail.ru>
2020-11-10 18:15:34 +0300

    refactor

--- src/main/java/ru/mail/polis/service/stasyanoi/server/helpers/Arguments.java
+++ src/main/java/ru/mail/polis/service/stasyanoi/server/helpers/Arguments.java
@@ -3,6 +3,9 @@ package ru.mail.polis.service.stasyanoi.server.helpers;
 import one.nio.http.HttpSession;
 import one.nio.http.Request;
 
+/**
+ * Arguments for requests.
+ */
 public class Arguments {
 
     private final String idParam;
