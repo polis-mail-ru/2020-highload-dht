84e3d22632efa1556d62f1fd4e634c8e732d3ff3
Stas Tarasov <iluvstrwrs@mail.ru>
2020-11-10 19:16:52 +0300

    refactor

--- src/main/java/ru/mail/polis/service/stasyanoi/server/CustomServer.java
+++ src/main/java/ru/mail/polis/service/stasyanoi/server/CustomServer.java
@@ -7,7 +7,6 @@ import one.nio.http.Path;
 import one.nio.http.Request;
 import one.nio.http.RequestMethod;
 import one.nio.http.Response;
-import org.javatuples.Pair;
 import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.dao.DAO;
 import ru.mail.polis.service.Mapper;
