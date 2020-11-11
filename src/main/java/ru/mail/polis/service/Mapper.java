2dcf86b3851e27d06d22ace3b32d7c4763d3cbec
Stas Tarasov <iluvstrwrs@mail.ru>
2020-11-11 16:18:23 +0300

    method fix

--- src/main/java/ru/mail/polis/service/Mapper.java
+++ src/main/java/ru/mail/polis/service/Mapper.java
@@ -18,12 +18,20 @@ public final class Mapper {
      */
     @NotNull
     public static byte[] toBytes(final ByteBuffer buffer) {
+        final int oldPosition = buffer.position();
         final byte[] bytes = new byte[buffer.limit()];
+        buffer.rewind();
         buffer.get(bytes);
-        buffer.clear();
+        buffer.position(oldPosition);
         return bytes;
     }
 
+    /**
+     * Create a byte buffer from byte array.
+     *
+     * @param bytes - input byte array.
+     * @return - created byte buffer.
+     */
     public static ByteBuffer fromBytes(final byte[] bytes) {
         return ByteBuffer.wrap(bytes);
     }
