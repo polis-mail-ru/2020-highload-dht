b61e4a8822c9ff7a55292b9caf5ae29f735b1a4e
Stas Tarasov <iluvstrwrs@mail.ru>
2020-11-14 12:07:59 +0300

    added basic session for streaming

--- src/main/java/ru/mail/polis/service/stasyanoi/StreamingSession.java
+++ src/main/java/ru/mail/polis/service/stasyanoi/StreamingSession.java
@@ -2,7 +2,6 @@ package ru.mail.polis.service.stasyanoi;
 
 import one.nio.http.HttpServer;
 import one.nio.http.HttpSession;
-import one.nio.http.Request;
 import one.nio.http.Response;
 import one.nio.net.Socket;
 import ru.mail.polis.Record;
@@ -18,29 +17,53 @@ public class StreamingSession extends HttpSession {
         super(socket, server);
     }
 
-    public void setRocksIterator(Iterator<Record> rocksIterator) {
+    public void setRocksIterator(Iterator<Record> rocksIterator) throws IOException {
         this.rocksIterator = rocksIterator;
-        this.rocksIterator.next();
+        final Response response = Util.responseWithNoBody(Response.OK);
+        response.addHeader("Transfer-Encoding: chunked");
+        writeResponse(response,false);
     }
 
     @Override
     public synchronized void sendResponse(Response response) throws IOException {
-        Request handling = this.handling;
-        if (handling == null) {
-            throw new IOException("Out of order response");
+        super.sendResponse(response);
+    }
+
+    protected void writeResponse(Response response, boolean includeBody) throws IOException {
+        byte[] bytes = response.toBytes(includeBody);
+        super.write(bytes, 0, bytes.length);
+
+    }
+
+    @Override
+    protected void processWrite() throws Exception {
+        super.processWrite();
+
+        if (rocksIterator != null) {
+            next();
+        }
+    }
+
+    private synchronized void next() throws IOException {
+        while (rocksIterator.hasNext() && queueHead == null) {
+            Record next = rocksIterator.next();
+//            byte[] key = Mapper.toBytes(next.getKey());
+//            byte[] value = Mapper.toBytes(next.getValue());
+//            byte[] data = new byte[key.length + 1 + value.length];
+//            System.arraycopy(key, 0, data, 0, key.length);
+            //data[key.length] = "\n".getBytes(StandardCharsets.US_ASCII);
         }
 
-        server.incRequestsProcessed();
+        if (rocksIterator.hasNext()) {
+            return;
+        }
 
         String connection = handling.getHeader("Connection: ");
         boolean keepAlive = handling.isHttp11()
                 ? !"close".equalsIgnoreCase(connection)
                 : "Keep-Alive".equalsIgnoreCase(connection);
-        response.addHeader(keepAlive ? "Connection: Keep-Alive" : "Connection: close");
-
-        writeResponse(response, handling.getMethod() != Request.METHOD_HEAD);
+        server.incRequestsProcessed();
         if (!keepAlive) scheduleClose();
-
         if ((this.handling = handling = pipeline.pollFirst()) != null) {
             if (handling == FIN) {
                 scheduleClose();
@@ -48,10 +71,5 @@ public class StreamingSession extends HttpSession {
                 server.handleRequest(handling, this);
             }
         }
-    }
-
-    protected void writeResponse(Response response, boolean includeBody) throws IOException {
-        byte[] bytes = response.toBytes(includeBody);
-        super.write(bytes, 0, bytes.length);
     }
 }
