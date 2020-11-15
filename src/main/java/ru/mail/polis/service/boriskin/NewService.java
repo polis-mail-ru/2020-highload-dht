74f62d1ecde4b0df5d4161155d0c6da9213da048
makaryb <boriskin.ma@edu.spbstu.ru>
2020-11-15 20:21:59 +0300

    Добавляет реализацию получения диапазона данных с помощью HTTP GET
    /v0/entities?start=<ID>[&end=<ID>]

--- src/main/java/ru/mail/polis/service/boriskin/NewService.java
+++ src/main/java/ru/mail/polis/service/boriskin/NewService.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.service.boriskin;
 
+import com.google.common.base.Charsets;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
@@ -8,14 +9,19 @@ import one.nio.http.Param;
 import one.nio.http.Path;
 import one.nio.http.Request;
 import one.nio.http.Response;
+import one.nio.net.Socket;
 import one.nio.server.AcceptorConfig;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.mail.polis.Record;
 import ru.mail.polis.dao.DAO;
+import ru.mail.polis.dao.boriskin.NewDAO;
 import ru.mail.polis.service.Service;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -63,6 +69,8 @@ public class NewService extends HttpServer implements Service {
     @NotNull
     private final ReplicaWorker replicaWorker;
 
+    @NotNull private final NewDAO dao;
+
     /**
      * Конструктор {@link NewService}.
      *
@@ -80,7 +88,7 @@ public class NewService extends HttpServer implements Service {
                 Runtime.getRuntime().availableProcessors(),
                 new ThreadFactoryBuilder().setNameFormat("worker-%d").build());
         this.topology = topology;
-
+        this.dao = (NewDAO) dao;
         final Set<String> nodeSet = topology.all();
         this.defaultReplicaFactor = ReplicaFactor.from(nodeSet.size());
 
@@ -119,7 +127,7 @@ public class NewService extends HttpServer implements Service {
             @Param("replicas") final String replicas,
             final HttpSession httpSession,
             final Request request) {
-        if (idNotValid(id, httpSession)) {
+        if (inputNotValid(id, httpSession)) {
             return;
         }
         final ReplicaFactor replicationFactor;
@@ -140,7 +148,58 @@ public class NewService extends HttpServer implements Service {
         runExecutorService(httpSession, request, replicationFactor);
     }
 
-    private boolean idNotValid(
+    /**
+     * Получение диапазона данных с помощью HTTP GET
+     * /v0/entities?start="ID"[&end="ID"],
+     * который возвращает:
+     *  - статус код 200 OK;
+     *  - возможно пустой отсортированный (по ключу) набор ключей и значений
+     *      в диапазоне ключей от обязательного start (включая)
+     *      до опционального end (не включая);
+     *  - используется Chunked transfer encoding;
+     *  - чанки в формате "key"\n"value".
+     * Диапазон должен отдаваться в потоковом режиме
+     * без формирования всего ответа в памяти.
+     *
+     * @param start обязательный
+     * @param end опциональный
+     * @param httpSession сессия
+     * @param request данные
+     */
+    @Path("/v0/entities")
+    public void entities(
+            @Param("start") final String start,
+            @Param("end") final String end,
+            final HttpSession httpSession,
+            final Request request) {
+        if (inputNotValid(start, httpSession)) {
+            return;
+        }
+        if (request.getMethod() != Request.METHOD_GET) {
+            logger.error("Неподдерживаемый метод");
+            resp(httpSession, new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY));
+            return;
+        }
+        try {
+            final Iterator<Record> records =
+                    dao.range(
+                            ByteBuffer.wrap(start.getBytes(Charsets.UTF_8)),
+                            end == null || end.isEmpty()
+                                    ? null : ByteBuffer.wrap(end.getBytes(Charsets.UTF_8)));
+            ((NewSession) httpSession).stream(records);
+        } catch (IOException ioException) {
+            logger.error("Внутренняя ошибка сервера");
+            resp(httpSession, new Response(Response.INTERNAL_ERROR, Response.EMPTY));
+        }
+    }
+
+    @Override
+    public HttpSession createSession(
+            final Socket socket) {
+        return new NewSession(socket, this);
+    }
+
+    private boolean inputNotValid(
             @Param(value = "id", required = true) final String id,
             @NotNull final HttpSession httpSession) {
         if (id == null || id.isEmpty()) {
