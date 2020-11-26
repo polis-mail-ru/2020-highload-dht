9350fb835d857dddc1af2c97edd68552c6508fa6
Andrey.Kovalkov <AndreyVladimirov1ch@yandex.ru>
2020-11-26 18:56:39 +0300

    remove bytes allocation

--- src/main/java/ru/mail/polis/service/kovalkov/ranges/StreamingSession.java
+++ src/main/java/ru/mail/polis/service/kovalkov/ranges/StreamingSession.java
@@ -59,13 +59,16 @@ public class StreamingSession extends HttpSession {
 
     @NotNull
     private static byte[] preparedChunk(@NotNull final Record currentRecord) {
-        final var key = BufferConverter.unfoldToBytes(currentRecord.getKey());
-        final var value = BufferConverter.unfoldToBytes(currentRecord.getValue());
-        final var dataSize = key.length + SPLIT.length + value.length;
+        final var dataSize = currentRecord.getKey().limit() + SPLIT.length + currentRecord.getValue().limit();
         final var hexLength = Integer.toHexString(dataSize).getBytes(Charset.defaultCharset());
         final var chunk = new byte[hexLength.length + CRLF.length + dataSize + CRLF.length];
         final var chunkBuffer = ByteBuffer.wrap(chunk);
-        chunkBuffer.put(hexLength).put(CRLF).put(key).put(SPLIT).put(value).put(CRLF);
+        chunkBuffer.put(hexLength)
+                    .put(CRLF)
+                    .put(currentRecord.getKey())
+                    .put(SPLIT)
+                    .put(currentRecord.getValue())
+                    .put(CRLF);
         return chunk;
     }
 
