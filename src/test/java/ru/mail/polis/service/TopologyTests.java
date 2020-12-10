83927273497c667410bc80506e07273587eea3e5
gogun <gusarov2000@gmail.com>
2020-12-10 23:18:18 +0300

    review fix

--- src/test/java/ru/mail/polis/service/TopologyTests.java
+++ src/test/java/ru/mail/polis/service/TopologyTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.mail.polis.service.gogun.ConsistentHashing;
 import ru.mail.polis.service.gogun.Hashing;
-import ru.mail.polis.service.gogun.ServiceUtils;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -23,7 +22,7 @@ public class TopologyTests extends ClusterTestBase {
 
     @Override
     int getClusterSize() {
-        return 9;
+        return 3;
     }
 
     //тест проверяет детерминированность нод для репликации
@@ -60,7 +59,7 @@ public class TopologyTests extends ClusterTestBase {
 
             for (int i : distr.values()) {
                 int diff = Math.abs(eachNodeNum - i);
-                float maximumDeviation = eachNodeNum * 0.4f;
+                float maximumDeviation = eachNodeNum * 0.2f;
                 Assertions.assertTrue(diff < maximumDeviation);
             }
         }
@@ -69,7 +68,7 @@ public class TopologyTests extends ClusterTestBase {
     //тест проверяет распределение ключей по нодам при репликейшн факторе больше 1
     @Test
     void distributionTestMany() {
-        final int replicationFactor = 5;
+        final int replicationFactor = 2;
         for (final String node : nodes) {
             final ConsistentHashing topology = new ConsistentHashing(Arrays.asList(nodes), node, VNODES);
             final Map<String, Integer> distr = new HashMap<>();
@@ -89,7 +88,7 @@ public class TopologyTests extends ClusterTestBase {
 
             for (int i : distr.values()) {
                 int diff = Math.abs(eachNodeNum - i);
-                float maximumDeviation = eachNodeNum * 0.4f;
+                float maximumDeviation = eachNodeNum * 0.2f;
                 Assertions.assertTrue(diff < maximumDeviation);
             }
         }
@@ -98,35 +97,26 @@ public class TopologyTests extends ClusterTestBase {
     //тест проверяет, что при добавлении ноды в кластер не происходит полной миграции ключей
     @Test
     void addNodeTest() {
-        final String nodeToAdd = nodes[nodes.length - 1];
-        final List<String> mutableNodes = new ArrayList<>(Arrays.asList(nodes));
-        mutableNodes.remove(nodeToAdd);
+        final List<String> nodesNotFull = new ArrayList<>(Arrays.asList(nodes));
+        nodesNotFull.remove(nodes.length - 1);
+        final List<String> nodesFull = new ArrayList<>(Arrays.asList(nodes));
         final String node = nodes[0];
-        final ConsistentHashing topologyNotfull = new ConsistentHashing(mutableNodes, node, VNODES);
-        final Map<ByteBuffer, String> keyNodeNotFull = new HashMap<>();
-        for (int i = 0; i < KEYS; i++) {
-            final ByteBuffer key = ServiceUtils.getBuffer(new byte[]{(byte) i});
-            final String nodeForKey = topologyNotfull.primaryFor(key, 1).toArray(new String[0])[0];
-            keyNodeNotFull.put(key, nodeForKey);
-        }
-
-        mutableNodes.add(nodeToAdd);
-        final ConsistentHashing topologyFull = new ConsistentHashing(mutableNodes, node, VNODES);
-        final Map<ByteBuffer, String> keyNodeFull = new HashMap<>();
-        for (int i = 0; i < KEYS; i++) {
-            final ByteBuffer key = ServiceUtils.getBuffer(new byte[]{(byte) i});
-            final String nodeForKey = topologyFull.primaryFor(key, 1).toArray(new String[0])[0];
-            keyNodeFull.put(key, nodeForKey);
-        }
+        final ConsistentHashing topologyNotFull = new ConsistentHashing(nodesNotFull, node, VNODES);
+        final ConsistentHashing topologyFull = new ConsistentHashing(nodesFull, node, VNODES);
         int migrationCounter = 0;
+
         for (int i = 0; i < KEYS; i++) {
-            final String notFullNode = keyNodeNotFull.get(ServiceUtils.getBuffer(new byte[]{(byte) i}));
-            final String fullNode = keyNodeFull.get(ServiceUtils.getBuffer(new byte[]{(byte) i}));
-            if (!fullNode.equals(notFullNode)) {
+            final ByteBuffer key = randomKeyBuffer();
+            final String nodeForKeyNotFull = topologyNotFull.primaryFor(key, 1).toArray(new String[0])[0];
+            final String nodeForKeyFull = topologyFull.primaryFor(key, 1).toArray(new String[0])[0];
+            if (!nodeForKeyNotFull.equals(nodeForKeyFull)) {
                 migrationCounter++;
             }
         }
 
-        Assertions.assertTrue(migrationCounter < KEYS / 2);
+        final int eachNodeNum = KEYS / nodes.length;
+        int diff = Math.abs(migrationCounter - eachNodeNum);
+        float maximumDeviation = eachNodeNum * 0.2f;
+        Assertions.assertTrue(diff < maximumDeviation);
     }
 }
